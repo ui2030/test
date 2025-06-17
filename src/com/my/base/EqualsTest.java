579a3b891271b6045375652c29d87a6722513a1f
kim <ui2030@daum.net>
2025-06-17 17:22:00 +0900

    1 day

---
+++ src/com/my/base/EqualsTest.java
@@ -0,0 +1,24 @@
+package com.my.base;
+
+public class EqualsTest {
+    public static void main(String[] args) {
+        test1();
+        test2();
+    }
+
+    static void test1() {
+        Object obj1 = new Object();
+        Object obj2 = obj1;         // 객체 복사
+        boolean result = obj1.equals(obj2);
+        System.out.println(result);
+        System.out.println(obj1.hashCode());
+        System.out.println(obj2.hashCode());
+    }
+    static void test2() {
+        String str = "홍길동";
+        String str2 = "홍길동";
+        System.out.println(str.equals(str2));
+        System.out.println(str == str2);
+
+    }
+}
--- src/com/my/base/TypeTest5.java
+++ src/com/my/base/TypeTest5.java
@@ -2,11 +2,15 @@ package com.my.base;
 
 public class TypeTest5 {
     public static void main(String[] args) {
-        String str = "let's learn Java";
+        String str = "let's learn Java";    // heap 영역의 string constrant pool
         String str2 = "let's learn Java";
+        System.out.println(System.identityHashCode(str));
+        System.out.println(System.identityHashCode(str2));
+        System.out.println();
 
-
-        //System.out.println(System.identityHashCode(str));
-        //System.out.println(System.identityHashCode(str2));
+        String str3 =  new String("let's learn Java");  // heap 영역에 저장
+        String str4 =  new String("let's learn Java");
+        System.out.println(System.identityHashCode(str3));
+        System.out.println(System.identityHashCode(str4));
     }
 }
